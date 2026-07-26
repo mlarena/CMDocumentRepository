308defe81bb2b6e00ff75639059954c4b77d8546
Vladimir <aaccportal@gmail.com>
2026-07-26 11:05:40 +0300

    доработки

--- CMDocumentRepository.Presentation/Models/ErrorViewModel.cs
+++ CMDocumentRepository.Presentation/Models/ErrorViewModel.cs
@@ -3,6 +3,5 @@ namespace CMDocumentRepository.Presentation.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
